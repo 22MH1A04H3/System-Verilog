06db7a6faef069bf0b467a0dc61f803a8db5815d
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-07-16 22:51:24 +0530

    Create Problem30.sv

---
+++ Constraint/Problem30.sv
@@ -0,0 +1,58 @@
+class packet;
+ 
+  //Constrain a 4-bit vector to have exactly 2 bits set.
+  rand bit [3:0]v;
+  constraint range{ $countones(v) == 2;}
+ 
+  
+endclass
+packet p;
+module tb;
+  initial
+    begin
+     // packet p;
+      p=new();
+      repeat(5)
+        begin
+          assert(p.randomize());
+          $display("%0b",p.v);
+        end
+    end
+endmodule
+
+========================================================
+[2025-07-16 17:20:36 UTC] vlib work && vlog '-timescale' '1ns/1ns' design.sv testbench.sv  && vsim -c -do "vsim +access+r; run -all; exit"  
+VSIMSA: Configuration file changed: `/home/runner/library.cfg'
+ALIB: Library "work" attached.
+work = /home/runner/work/work.lib
+MESSAGE "Unit top modules: tb."
+SUCCESS "Compile success 0 Errors 0 Warnings  Analysis time: 0[s]."
+done
+# Aldec, Inc. Riviera-PRO version 2023.04.112.8911 built for Linux64 on May 12, 2023.
+# HDL, SystemC, and Assertions simulator, debugger, and design environment.
+# (c) 1999-2023 Aldec, Inc. All rights reserved.
+# ELBREAD: Elaboration process.
+# ELBREAD: Elaboration time 0.0 [s].
+# KERNEL: Main thread initiated.
+# KERNEL: Kernel process initialization phase.
+# ELAB2: Elaboration final pass...
+# ELAB2: Create instances ...
+# KERNEL: Time resolution set to 1ns.
+# ELAB2: Create instances complete.
+# SLP: Started
+# SLP: Elaboration phase ...
+# SLP: Elaboration phase ... skipped, nothing to simulate in SLP mode : 0.0 [s]
+# SLP: Finished : 0.0 [s]
+# ELAB2: Elaboration final pass complete - time: 0.0 [s].
+# KERNEL: Warning: You are using the Riviera-PRO EDU Edition. The performance of simulation is reduced.
+# KERNEL: Warning: Contact Aldec for available upgrade options - sales@aldec.com.
+# KERNEL: Kernel process initialization done.
+# Allocation: Simulator allocated 4745 kB (elbread=455 elab2=4148 kernel=142 sdf=0)
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL: 1001
+# KERNEL: 1001
+# KERNEL: 101
+# KERNEL: 1001
+# KERNEL: 1001
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
