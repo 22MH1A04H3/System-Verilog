d909477c26296aff4cc31306bfe7edb016fafe56
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-08-15 13:48:35 +0530

    Update 2_and_4_state_data_types.sv

--- Basics/2_and_4_state_data_types.sv
+++ Basics/2_and_4_state_data_types.sv
@@ -46,3 +46,35 @@ module tb;
       
     end
 endmodule
+
+
+
+
+_________________________________________________________________
+[2025-08-15 08:14:32 UTC] xrun -Q -unbuffered '-timescale' '1ns/1ns' '-sysv' '-access' '+rw' design.sv testbench.sv  
+TOOL:	xrun	23.09-s001: Started on Aug 15, 2025 at 04:14:33 EDT
+xrun: 23.09-s001: (c) Copyright 1995-2023 Cadence Design Systems, Inc.
+	Top level design units:
+		tb
+      $display("j=%b",j);
+                      |
+xmelab: *W,REALCV (./testbench.sv,41|22): Real argument converted to integer for (%b).
+Loading snapshot worklib.tb:sv .................... Done
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+a=0
+b=10000001
+c=10000100100001001101011000001001
+d=101011001100011
+e=110101110010111101100001101
+f=1
+g=1
+h=10001001001101110101001000010010
+i=10
+j=00000000000000000000000000000011
+k=30
+l=1
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Aug 15, 2025 at 04:14:34 EDT  (total: 00:00:01)
+Done
