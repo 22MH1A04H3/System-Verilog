cbe0eafe52bb88f4da55fbc8cc1249278b68ae77
Dasu Koda <119719005+22MH1A04H3@users.noreply.github.com>
2025-10-03 10:22:16 +0530

    Update class.sv

--- Oops..!/class.sv
+++ Oops..!/class.sv
@@ -1,1 +1,25 @@
-Uploading......!
+class das;
+  rand bit [4:0]data;
+  constraint range{data inside{[1:10]};
+                  }
+endclass
+das v;
+
+module tb;
+  initial
+    begin
+     
+      repeat(10)
+        begin
+           v=new();
+          v.randomize();
+          $display("data = %0d ",v.data);
+          #5;
+          
+        end
+      #10;
+      $stop;
+      
+      
+    end
+endmodule
